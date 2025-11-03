547820bd2660d9143b0a738dabfceb1c06ae58af
Lewis-mbui <lewmugo14479@gmail.com>
2025-11-03 07:46:40 +0300

    remove editing and check features on pending list items

--- scripts/pending-todos.js
+++ scripts/pending-todos.js
@@ -30,13 +30,9 @@ export function pendingTodosHTML() {
     html += `
       <li class="todo">
         <div class="todo__item">
-          <input id="check-todo" type="checkbox" />
           <label for="check-todo">${todo.text}</label>
         </div>
         <div class="todo__icons">
-          <svg class="icon icon-edit">
-            <use xlink:href="/assets/images/icons.svg#pen-solid-full"></use>
-          </svg>
           <svg class="icon icon-delete">
             <use xlink:href="/assets/images/icons.svg#trash-solid-full"></use>
           </svg>
